9eff892c3ace8f681704956e8cf3cfbb35bd00f2
ksparimala <kakularapuparimala@gmail.com>
2021-04-16 22:40:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Foodbox from './components/FoodBox/FoodBoxComponent'
+import Title from './components/TitileBar/Title'
+
+
+function App() {
+  return (<div>
+          <Title />
+          <Foodbox />
+  </div>);
+}
+
+export default App;
